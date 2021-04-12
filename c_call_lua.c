e41d5208446c549f6c7ae1c48d32ad015024a782
lch <412288482@qq.com>
2021-04-12 16:37:13 +0800

    add more test

--- c_call_lua.c
+++ c_call_lua.c
@@ -9,15 +9,20 @@ void test_add() {
 
 	int ret = luaL_dofile(L, "call_lua.lua");
 	printf("luaL_dofile ret:%d\n", ret);
-
+	if (ret != 0)
+		goto exit;
+	
 	lua_getfield(L, -1, "add");
 	lua_pushinteger(L, 5);
 	lua_pushinteger(L, 6);
-	int ret2 = lua_pcall(L, 2, 1, 0);
-	printf("ret2:%d\n", ret2);
-	if (ret2 == 0)
-		printf("result:%d\n", (int)lua_tointeger(L, -1));
+	ret = lua_pcall(L, 2, 1, 0);
+	printf("lua_pcall ret:%d\n", ret);
+	if (ret != 0)
+		goto exit;
 
+	printf("result:%d\n", (int)lua_tointeger(L, -1));
+
+exit:
 	lua_close(L);
 }
 
